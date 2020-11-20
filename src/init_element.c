b9fb7fcc2fcdfab3626e8a8ecdcbbcc9114a9a6a
pierre1754 <pierre1754@outlook.com>
2020-11-20 10:23:47 +0100

    music added

--- src/init_element.c
+++ src/init_element.c
@@ -11,6 +11,8 @@ void init_origin_img(void)
 {
     engine_t *engine = get_engine();
 
+    sfMusic_play(GET_ASSET_AMBIANCE(engine));
+    sfMusic_setLoop(GET_ASSET_AMBIANCE(engine), sfTrue);
     sfSprite_setOrigin(GET_OBJ_ENV_AIM_IMG(engine), (sfVector2f) {256, 256});
-    sfSprite_setOrigin(GET_OBJ_ENV_CANON_IMG(engine), (sfVector2f) {104, 0});
+    sfSprite_setOrigin(GET_OBJ_ENV_CANON_IMG(engine), (sfVector2f) {100, 0});
 }
